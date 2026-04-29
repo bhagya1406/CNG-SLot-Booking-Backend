206ade2ea7dbd1e795ada1e88ad3b6f815c589d2
Bhagyashri Shintre <bsshintre77@gmail.com>
2026-04-30 00:48:30 +0530

    cors added

--- src/main/java/com/example/cng_booking/controllers/AuthenticationController.java
+++ src/main/java/com/example/cng_booking/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     @Autowired
